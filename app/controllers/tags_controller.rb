8b40664290a0818b098d4a5794631f4e0b8c341c
msl12 <2496227361@qq.com>
2017-03-09 21:35:31 +0800

    optimize tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
 	def show
-		@blogs = Blog.tagged_with(params[:name]).paginate(page: params[:page], per_page: 2)
+		@blogs = Blog.tagged_with(params[:name]).order('created_at DESC').paginate(page: params[:page], per_page: 2)
 		raise ActiveRecord::RecordNotFound if @blogs.blank?
 		@total = 1 + @blogs.count / 2
 	end
